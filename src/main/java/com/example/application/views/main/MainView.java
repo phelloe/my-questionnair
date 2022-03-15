5b3d579abf4f1da0e63385fc4eaaeb1a850e5860
phelloe <github@uaaargh.de>
2022-03-15 20:34:08 +0100

    Removed unused inputs

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -20,9 +20,7 @@ import com.vaadin.flow.router.Route;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @PageTitle("Main")
 @Route(value = "")
